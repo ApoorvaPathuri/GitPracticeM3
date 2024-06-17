04c1239773b1a4ff89919132a0d14fb204ea8887
asc <asc@DESKTOP-P0URRV6>
2024-06-17 11:56:43 +0530

    Added tests

--- src/test/java/tests/Test2.java
+++ src/test/java/tests/Test2.java
@@ -1,5 +1,13 @@
 package tests;
+	import org.testng.Reporter;
+	import org.testng.annotations.Test;
 
-public class Test2 {
+	public class Test2 {
+		@Test
+		public void sampleTest(){
+			Reporter.log("Test2-sampleTest",true);	
+			}
 
-}
+	}
+
+
--- src/test/java/tests/Test3.java
+++ src/test/java/tests/Test3.java
@@ -1,5 +1,13 @@
 package tests;
+	import org.testng.Reporter;
+	import org.testng.annotations.Test;
 
-public class Test3 {
+	public class Test3 {
+		@Test
+		public void sampleTest(){
+			Reporter.log("Test3-sampleTest",true);	
+			}
 
-}
+	}
+
+
